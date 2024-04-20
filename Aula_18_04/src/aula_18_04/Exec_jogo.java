ef9fe0f9319ff854361bd969f22b4eef12bbd414
Vinicius Santos <vsantosdesousa82@gmail.com>
2024-04-20 18:15:52 -0300

    Classes
    Criei a Classe Main

--- Aula_18_04/src/aula_18_04/Exec_jogo.java
+++ Aula_18_04/src/aula_18_04/Exec_jogo.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package aula_18_04;
+import java.util.Scanner;
 
-/**
- *
- * @author vsant
- */
+import java.util.Random;
+
 public class Exec_jogo {
+    public static void main(String[] args) {
+        
+    }
     
 }
