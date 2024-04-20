ad9d187d5137ccc6787b5d0feac64cad7566f899
Vinicius Santos <vsantosdesousa82@gmail.com>
2024-04-20 18:31:02 -0300

    Instanciei
    Criei instancias de Randon e Scanner

--- Aula_18_04/src/aula_18_04/Exec_jogo.java
+++ Aula_18_04/src/aula_18_04/Exec_jogo.java
@@ -7,6 +7,10 @@ public class Exec_jogo {
     public static void main(String[] args) {
         
         int numero=0, randon = 0;
+        
+        Scanner objscan =new Scanner(System.in);
+        Random objram = new Random();
+        
     }
     
 }
